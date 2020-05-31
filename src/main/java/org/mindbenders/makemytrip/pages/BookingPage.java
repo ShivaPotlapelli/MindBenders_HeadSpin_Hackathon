b5d17f9dd78d5855c5cfab25057884457a87819e
Rajaneeshkumar <rajaneeshkumar12@gmail.com>
2020-05-31 20:02:37 +0530

    Hotel rooms persons changes

--- src/main/java/org/mindbenders/makemytrip/pages/BookingPage.java
+++ src/main/java/org/mindbenders/makemytrip/pages/BookingPage.java
@@ -10,12 +10,39 @@ public class BookingPage extends TestBase {
     private static final By enterCity= By.className("react-autosuggest__input");
     private static final By checkin= By.cssSelector("[data-cy='checkin']");
     private static final By datepicker= By.className("DayPicker-Day");
+    private static final By guest= By.cssSelector("[data-cy='guest']");
+    private static final By roomMembers= By.cssSelector(".guestCounter >li");
+    private static final By addRoom= By.cssSelector("[data-cy='addAnotherRoom']");
+    private static final By apply= By.cssSelector("[data-cy='submitGuest']");
+    private static final By travellingFor= By.cssSelector("[data-cy='travelForText']");
+    private static final By travellingForWork= By.cssSelector("[data-cy='travelFor-Work']");
+    private static final By search= By.cssSelector("[data-cy='submit']");
 
-    public void Booking(String area,String month,String day) {
+    public void Booking(String area,String checkInMonth,String checkInDay,String checkOutMonth,String checkOutDay) {
         clickElement(hotel);
+        //select city
         clickElement(city);
         sendText(enterCity,area);
+        //checkin date
         clickElement(checkin);
-        datePicker(datepicker,month,day);
+        datePicker(datepicker,checkInMonth,checkInDay);
+        //checkout date
+        datePicker(datepicker,checkOutMonth,checkOutDay);
+        //select Adult Guest
+        clickElement(guest);
+        guestSelect(roomMembers,"adults-2");
+        //select Children Guest
+        guestSelect(roomMembers,"children-2");
+        //addRoom
+        clickElement(addRoom);
+        //select Adult Guest
+        clickElement(guest);
+        guestSelect(roomMembers,"adults-2");
+        //select Children Guest
+        guestSelect(roomMembers,"children-2");
+        clickElement(apply);
+        clickElement(travellingFor);
+        clickElement(travellingForWork);
+        clickElement(search);
     }
 }
